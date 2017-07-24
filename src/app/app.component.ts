469624801ee6806d1cb79a6aad5f05697c8e4180
jarias <jarias@koghi.com>
2017-07-24 08:30:28 -0500

    Cambios anteriores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+/*Nooo*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Componente Base Principal';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FruitComponent } from './fruit/fruit.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitComponent,
+    EmployeeComponent
   ],
-  imports: [
+  imports: [//Cargar modulos del framework de forma global
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //Componente que se cargará de primeras (Principal)
 })
 export class AppModule { }
